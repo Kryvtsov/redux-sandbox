aaebc03e8637888667c054ba00641b81b6c6e099
Vladymyr <vladymyr13@gmail.com>
2019-01-09 23:49:59 +0200

    Action creator

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import {createStore} from "redux";
 
+
 const reducer = (state = 0, action) => {
     switch (action.type) {
+        case 'RND':
+            return state + action.payload;
         case 'INC':
             return state + 1;
         case 'DEC':
             return state - 1;
+        case 'ZERO':
+            return state = 0;
         default:
             return state;
     }
@@ -13,15 +18,29 @@ const reducer = (state = 0, action) => {
 
 const store = createStore(reducer);
 
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'});
+const rnd = (payload) => ({type: 'RND', payload});
+const zero = () => ({type: 'ZERO'});
 
+
+document.getElementById('rnd')
+    .addEventListener('click', () => {
+        const payload = Math.floor(Math.random()*9);
+        store.dispatch(rnd(payload));
+    });
 
 document.getElementById('inc')
     .addEventListener('click', () =>
-        store.dispatch({type: 'INC'}));
+        store.dispatch(inc()));
 
 document.getElementById('dec')
     .addEventListener('click', () =>
-        store.dispatch({type: 'DEC'}));
+        store.dispatch(dec()));
+
+document.getElementById('zero')
+    .addEventListener('click', () =>
+        store.dispatch(zero()));
 
 const update = () => {
     document.getElementById('counter')
